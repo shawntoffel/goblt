5c6386d7df96bc8cb70ecdaa9ea2a6e1aa167355
Shawn Toffel <shawn.toffel@gmail.com>
2022-04-06 05:14:42 +0000

    Added methods for named & flat named ballots

--- election.go
+++ election.go
@@ -13,3 +13,66 @@ type Ballot struct {
 	Count       int
 	Preferences [][]int
 }
+
+type NamedBallot struct {
+	Count       int
+	Preferences [][]string
+}
+
+type FlatNamedBallot struct {
+	Count       int
+	Preferences []string
+}
+
+func (e *Election) NamedWithdrawn() []string {
+	namedWithdrawn := make([]string, len(e.Withdrawn))
+
+	for i, index := range e.Withdrawn {
+		namedWithdrawn[i] = e.Candidates[index-1]
+	}
+
+	return namedWithdrawn
+}
+
+func (e *Election) NamedBallots() []NamedBallot {
+	namedBallots := make([]NamedBallot, len(e.Ballots))
+
+	for i, ballot := range e.Ballots {
+		namedBallot := NamedBallot{
+			Count:       ballot.Count,
+			Preferences: make([][]string, len(ballot.Preferences)),
+		}
+
+		for j, preferences := range ballot.Preferences {
+			namedBallot.Preferences[j] = make([]string, len(preferences))
+
+			for k, preference := range preferences {
+				namedBallot.Preferences[j][k] = e.Candidates[preference-1]
+			}
+		}
+		namedBallots[i] = namedBallot
+	}
+
+	return namedBallots
+}
+
+func (e *Election) FlatNamedBallots() []FlatNamedBallot {
+	namedballots := e.NamedBallots()
+	flatNamedBallots := make([]FlatNamedBallot, len(namedballots))
+
+	for i, ballot := range namedballots {
+		names := []string{}
+		for _, preferences := range ballot.Preferences {
+			for _, preference := range preferences {
+				names = append(names, preference)
+			}
+		}
+
+		flatNamedBallots[i] = FlatNamedBallot{
+			Count:       ballot.Count,
+			Preferences: names,
+		}
+	}
+
+	return flatNamedBallots
+}
--- parser_test.go
+++ parser_test.go
@@ -7,11 +7,7 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	data := loadFileData("simple.blt")
-
-	p := NewParser(strings.NewReader(data))
-
-	result, err := p.Parse()
+	result, err := parse("simple.blt")
 	if err != nil {
 		t.Error(err)
 	}
@@ -19,9 +15,66 @@ func TestParse(t *testing.T) {
 	assertInt(t, 6, result.NumCandidates, "candidates")
 	assertInt(t, 3, result.NumSeats, "seats")
 	assertIntSlice(t, []int{6}, result.Withdrawn, "withdrawn")
+
 	assertInt(t, 5, len(result.Ballots), "ballots")
+
+	ballots := result.Ballots
+	assert2dIntSlice(t, [][]int{{1, 2}, {3}}, ballots[0].Preferences, "preferences")
+	assert2dIntSlice(t, [][]int{{2}, {1}, {3}}, ballots[1].Preferences, "preferences")
+	assert2dIntSlice(t, [][]int{{3}}, ballots[2].Preferences, "preferences")
+	assert2dIntSlice(t, [][]int{{4}}, ballots[3].Preferences, "preferences")
+	assert2dIntSlice(t, [][]int{{5}}, ballots[4].Preferences, "preferences")
+
 	assertInt(t, 6, len(result.Candidates), "candidate names")
 	assertString(t, "Test", result.Title, "title")
+}
+
+func TestNamedBallots(t *testing.T) {
+	result, err := parse("simple.blt")
+	if err != nil {
+		t.Error(err)
+	}
+
+	named := result.NamedBallots()
+	assertInt(t, 5, len(named), "ballots")
+	assertInt(t, 2, len(named[0].Preferences), "preference")
+	assertInt(t, 3, len(named[1].Preferences), "preference")
+	assertInt(t, 1, len(named[2].Preferences), "preference")
+	assertInt(t, 1, len(named[3].Preferences), "preference")
+	assertInt(t, 1, len(named[4].Preferences), "preference")
+}
+
+func TestFlatNamedBallots(t *testing.T) {
+	result, err := parse("simple.blt")
+	if err != nil {
+		t.Error(err)
+	}
+
+	named := result.FlatNamedBallots()
+	assertInt(t, 5, len(named), "ballots")
+	assertInt(t, 3, len(named[0].Preferences), "preference")
+	assertInt(t, 3, len(named[1].Preferences), "preference")
+	assertInt(t, 1, len(named[2].Preferences), "preference")
+	assertInt(t, 1, len(named[3].Preferences), "preference")
+	assertInt(t, 1, len(named[4].Preferences), "preference")
+}
+
+func TestNamedWithdrawn(t *testing.T) {
+	result, err := parse("simple.blt")
+	if err != nil {
+		t.Error(err)
+	}
+
+	named := result.NamedWithdrawn()
+	assertInt(t, 1, len(named), "withdrawn")
+	assertString(t, "Frank", named[0], "withdrawn name")
+
+}
+
+func parse(filename string) (*Election, error) {
+	data := loadFileData(filename)
+	p := NewParser(strings.NewReader(data))
+	return p.Parse()
 }
 
 func loadFileData(filename string) string {
@@ -38,6 +91,12 @@ func assertInt(t *testing.T, expected, got int, name string) {
 func assertString(t *testing.T, expected, got string, name string) {
 	if expected != got {
 		t.Errorf("expected %s %s, got %s", expected, name, got)
+	}
+}
+
+func assert2dIntSlice(t *testing.T, expected, got [][]int, name string) {
+	for i, val := range got {
+		assertIntSlice(t, val, expected[i], name)
 	}
 }
 
